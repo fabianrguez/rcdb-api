4b44e31dd0598d0e2dfb7b619f48d7ca3f85b3b8
Fabián Rodríguez <fabian.rguezg@gmail.com>
2023-01-10 22:01:03 +0000

    chore(all) remove dist folder and add deploy-api workflow

--- dist/lib/decorators/controller-decorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const types_1 = require("../types");
-function Controller(basePath = '/') {
-    return (target) => {
-        Reflect.defineMetadata(types_1.MetadataKeys.BASE_PATH, basePath, target);
-    };
-}
-exports.default = Controller;
